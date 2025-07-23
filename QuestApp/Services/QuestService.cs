5e47062a40851f998f8758208d658a72e14f1fcf
ArBiTheG <56531440+ArBiTheG@users.noreply.github.com>
2025-07-23 14:20:36 +0300

    Добавлен сервис для загрузки данных с JSON файла

--- QuestApp/Services/QuestService.cs
+++ QuestApp/Services/QuestService.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using QuestApp.DataAccess;
+using QuestApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +11,19 @@ namespace QuestApp.Services
 {
     internal class QuestService
     {
+        IDataProvider<Test> _dataProvider;
 
+        public QuestService()
+        {
+            _dataProvider = new JSONDataProvider<Test>("quests.json");
+        }
+
+        public Test GetTest()
+        {
+            var test = _dataProvider.LoadData()?.ToArray();
+            if (test != null)
+                return test[0];
+            return new Test();
+        }
     }
 }
