d42ceceb648fb0c3ab3e4bc98b67246fca4e7cec
ArBiTheG <56531440+ArBiTheG@users.noreply.github.com>
2025-07-23 15:17:51 +0300

    Добавлен Mock для данных Json

--- QuestApp/Services/QuestService.cs
+++ QuestApp/Services/QuestService.cs
@@ -16,6 +16,8 @@ namespace QuestApp.Services
         public QuestService()
         {
             _dataProvider = new JSONDataProvider<Test>("quests.json");
+
+            _dataProvider.SaveData(MockJsonData());
         }
 
         public Test GetTest()
@@ -25,6 +27,49 @@ namespace QuestApp.Services
                 if (test.Length > 0)
                     return test[0];
             return new Test("Квест не загружен","Не удалось загрузить квест, поскольку был не найден");
+        }
+
+        public List<Test> MockJsonData()
+        {
+            return new List<Test>()
+            {
+                new Test("Test", "DescriptionTest")
+                {
+                    Questions = new List<Question>(){
+                        new Question(Guid.Parse("f8f19551-dbc8-45f2-892d-c898de996876"),"Question1", "DescriptionQuestion1")
+                        {
+                            Answers = new List<Answer>()
+                            {
+                                new Answer("Answer1", "DescriptionAnswer1")
+                                {
+                                    NavigateQuestion = Guid.Parse("24625bd9-6538-4084-ac89-11e91c86dc62")
+                                },
+                                new Answer("Answer2", "DescriptionAnswer2")
+                                {
+                                    NavigateQuestion = Guid.Parse("09690b6a-94d6-4992-a366-4d61a87efbc6")
+                                },
+                            }
+                        },
+
+                        new Question(Guid.Parse("24625bd9-6538-4084-ac89-11e91c86dc62"),"Question2", "DescriptionQuestion2")
+                        {
+                            Answers = new List<Answer>()
+                            {
+                                new Answer("Answer3","DescriptionAnswer3")
+                            }
+                        },
+
+                        new Question(Guid.Parse("09690b6a-94d6-4992-a366-4d61a87efbc6"),"Question3", "DescriptionQuestion3")
+                        {
+                            Answers = new List<Answer>()
+                            {
+                                new Answer("Answer4","DescriptionAnswer4")
+                            }
+                        }
+                    }
+                }
+            };
+
         }
     }
 }
