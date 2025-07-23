a6e3dd2f3463b3559da5e6f0ba47a1c192ad6ac0
ArBiTheG <56531440+ArBiTheG@users.noreply.github.com>
2025-07-23 13:27:26 +0300

    Создан тестовый интерфейс приложения

--- QuestApp/ViewModel/MainViewModel.cs
+++ QuestApp/ViewModel/MainViewModel.cs
@@ -1,6 +1,8 @@
-﻿using QuestApp.Services;
+﻿using QuestApp.Model;
+using QuestApp.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +13,24 @@ namespace QuestApp.ViewModel
     {
         QuestService _questService;
 
+        public Test Test { get; set; }
+        public Question CurrentQuestion { get; set; }
+
         public MainViewModel()
         {
             _questService = new QuestService();
+
+            CurrentQuestion = new Question("Test1", "TestDescription1");
+
+            CurrentQuestion.Answers = new ObservableCollection<Answer>()
+            {
+                new Answer("TestAnswer1", "TestAnswerDescription1"),
+                new Answer("TestAnswer2", "TestAnswerDescription2"),
+                new Answer("TestAnswer3", "TestAnswerDescription3"),
+                new Answer("TestAnswer4", "TestAnswerDescription4")
+            };
         }
+
+
     }
 }
